268260ea2f10aa1308c54e3d5fab53a6c0fcf50c
chumpon-asaneerat <chumponsenate@gmail.com>
2019-04-22 18:07:55 +0700

    Add some npm packages (joi, helmet).

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require('express')
+const helmet = require('helmet')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 
+const Joi = require('joi') // the object validation class.
+
 const app = new express();
 
 app.use(logger('dev'))
+app.use(helmet());
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use(cookieParser())
 
 app.get('/', (req, res) => {
-    //console.log(req.body)
-    //console.log(req.cookies)
-    res.send('received request..')
+    res.status(200).send('Welcome to test page.');
 })
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
     console.log('listen on port: 3000');
 })
