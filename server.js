dde3a9263986c28fb1835817ae00e3f1e49439ef
chumpon-asaneerat <chumponsenate@gmail.com>
2019-04-22 19:08:31 +0700

    Add simple custom middleware.

--- server.js
+++ server.js
@@ -9,16 +9,28 @@ const Joi = require('joi') // the object validation class.
 const app = new express();
 
 app.use(logger('dev'))
-app.use(helmet());
+app.use(helmet())
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
-    res.status(200).send('Welcome to test page.');
-})
+// new custom logger middleware
+function mylogger(req, res, next) {
+    let { method, originalUrl, protocol } = req
+    console.log(`${method} => ${protocol}://${req.get('host')}${originalUrl}`)
+    next()
+}
+
+app.use(mylogger) // use custom logger middleware
+
+// various routes.
+app.get('/', (req, res) => { res.status(200).send('Welcome to test page.'); })
+app.get('/about', (req, res) => { res.status(200).send('Welcome to about page.'); })
+app.get('/contact', (req, res) => { res.status(200).send('Welcome to contact page.'); })
+app.get('/protected/api', (req, res) => { res.status(200).send('Welcome to api page.'); })
+app.get('/my/profile', (req, res) => { res.status(200).send('Welcome to profile page.'); })
 
 const server = app.listen(3000, () => {
     console.log('listen on port: 3000');
